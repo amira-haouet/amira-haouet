13cf42df515ede84d130cfb6712b14e2d1abd995
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2022-01-06 14:49:03 +0100

    upate style of the letter

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 
+var text = document.querySelector('.division .letters');
+text.innerHTML = text.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.division .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 500
+  })
+  .add({
+    targets: '.division .line',
+    translateX: [0, document.querySelector('.division .letters').getBoundingClientRect()/*DOMRect*/.width + 20],
+
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.division .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 500,
+    offset: '-=775',
+    delay: (el, i) => 35 * (i+1)
+  }).add({
+    targets: '.division',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
